850285633706c7f1cd0ec13f1fe40010cb5bc08b
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-10-10 19:57:37 +0300

    first commit

---
+++ 1/1.js
@@ -0,0 +1,17 @@
+//Получение кнопки и иконок
+const iconChangeButton = document.querySelector(".icon-change-button");
+const buttonIcons = document.querySelectorAll(".button-icon");
+
+//Функция для смены иконки
+const changeIcon = (icons) => {
+    icons.forEach((el) => {
+        el.classList.toggle("hidden");
+    })
+}
+
+//Ивент по клику для смены иконки
+iconChangeButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    changeIcon(buttonIcons);
+});
+
